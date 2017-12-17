bff0cb5fa1871b614f781ccdf70240469d35f5ad
janelle-nomad <janelle.saige@gmail.com>
2017-12-17 18:28:37 -0500

    Updated the RestaurantTableViewController, allowing users to share or delete restaurants.

--- Foodie/RestaurantTableViewController.swift
+++ Foodie/RestaurantTableViewController.swift
@@ -50,6 +50,8 @@ class RestaurantTableViewController: UITableViewController {
         "Latin American", "Spanish", "Spanish", "Spanish", "British",
         "Thai"]
     
+      var restaurantIsVisited = Array(repeating: false, count: 21)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -86,53 +88,104 @@ class RestaurantTableViewController: UITableViewController {
         cell.thumbnailImageView.image = UIImage(named: restaurantImages[indexPath.row])
         cell.locationLabel.text = restaurantLocations[indexPath.row]
         cell.typeLabel.text = restaurantTypes[indexPath.row]
+        
+        //Creates an accessory, checkmark, sets it as an optional
+        //User visitation to the restaurant
+        cell.accessoryType = restaurantIsVisited[indexPath.row] ? .checkmark : .none
         return cell
     }
  
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Create an option menu as an action sheet
+        let optionMenu = UIAlertController(title: nil, message: "What do you want to do?", preferredStyle: .actionSheet)
+        
+        // Add actions to the menu
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        optionMenu.addAction(cancelAction)
+        
+        // Add Call action
+        let callActionHandler = { (action:UIAlertAction!) -> Void in
+            
+            let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry, the call feature is not available yet. Please retry later.", preferredStyle: .alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alertMessage, animated: true, completion: nil)
+            
+        }
+        
+        let callAction = UIAlertAction(title: "Call " + "123-000-\(indexPath.row)", style: .default, handler: callActionHandler)
+        optionMenu.addAction(callAction)
+        
+        // Check-in action
+        // Determine the check in title. If the selected restaurant has been checked (i.e.
+        // restaurantIsVisited[indexPath.row] returns true), the title will be set to "Undo Check in".
+        let checkInTitle = restaurantIsVisited[indexPath.row] ? "Undo Check in" : "Check in"
+        let checkInAction = UIAlertAction(title: checkInTitle, style: .default, handler: {
+            //update the value of the Bool array when a restaurant is checked
+            (action:UIAlertAction!) -> Void in
+            
+            let cell = tableView.cellForRow(at: indexPath)
+            
+            // Toggle check-in and undo-check-in (updates check-in), if restaurant visit cond. is met
+            self.restaurantIsVisited[indexPath.row] = self.restaurantIsVisited[indexPath.row] ? false : true
+            cell?.accessoryType = self.restaurantIsVisited[indexPath.row] ? .checkmark : .none
+        })
+        optionMenu.addAction(checkInAction)
+        
+        tableView.deselectRow(at: indexPath, animated: false)
+        
+        // Display the menu
+        present(optionMenu, animated: true, completion: nil)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    
+    //Provided by TableViewController, handles insertion and or deletion of data of a specific row,
+    //even without functionality, the delete button will appear on swipe.
+    override func tableView(_ tableView: UITableView, commit editingStyle:
+        UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        //If delete is selected then
         if editingStyle == .delete {
-            // Delete the row from the data source
+            restaurantNames.remove(at: indexPath.row)
+            restaurantLocations.remove(at: indexPath.row)
+            restaurantTypes.remove(at: indexPath.row)
+            restaurantImages.remove(at: indexPath.row)
+            restaurantIsVisited.remove(at: indexPath.row)
+        }
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    override func tableView(_ tableView: UITableView, editActionsForRowAt
+        indexPath: IndexPath) -> [UITableViewRowAction]? {
+        
+        
+        // Social Sharing Button
+        let shareAction = UITableViewRowAction(style:
+            UITableViewRowActionStyle.default, title: "Share", handler: { (action,
+                indexPath) -> Void in
+                let defaultText = "Just checking in at " +
+                    self.restaurantNames[indexPath.row]
+                let activityController = UIActivityViewController(activityItems:
+                    [defaultText], applicationActivities: nil)
+                self.present(activityController, animated: true, completion: nil)
+        })
+        //Delete button
+        let deleteAction = UITableViewRowAction(style:  UITableViewRowActionStyle.default, title: "Delete",handler: { (action,
+            indexPath) -> Void in
+            // Delete the row from the data source
+            self.restaurantNames.remove(at: indexPath.row)
+            self.restaurantLocations.remove(at: indexPath.row)
+            self.restaurantTypes.remove(at: indexPath.row)
+            self.restaurantIsVisited.remove(at: indexPath.row)
+            self.restaurantImages.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .fade)
+        })
+        
+        //Torquise share button
+        shareAction.backgroundColor = UIColor(red: 1/255.0, green: 173.0/255.0,
+                                              blue: 200/254.0, alpha: 1.0)
+        //Red delete button
+        deleteAction.backgroundColor = UIColor(red: 202.0/255.0, green: 0/0,
+                                               blue: 0/0, alpha: 1.0)
+        
+        return [deleteAction, shareAction]
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+    
